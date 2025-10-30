5a12d7995e138768a4006649022c21414f55382f
Joseph Levinskas <levinskasiv@gmail.com>
2025-10-30 23:23:55 +0000

    add getHumanChoice function which asks for input (not worrying about correct input right not) and returns the input

--- javascript.js
+++ javascript.js
@@ -3,9 +3,16 @@ function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     switch(choice) {
         case 0: return "rock";
-        case 1: return "scissors";
+        case 1: return "scissor";
         case 2: return "paper";
         default: return "?"
     }
 }
 
+function getHumanChoice() {
+    const input = prompt("Enter your choice:");
+    console.log("You entered:", input);
+    return input;
+}
+
+getHumanChoice();
