80c45cf1889e39b8fef111662f26f8cfe95369b4
Joseph Levinskas <levinskasiv@gmail.com>
2025-10-30 23:45:16 +0000

    Finished playgame and playround function so the game is played 5 times and displays current round score and game results

--- javascript.js
+++ javascript.js
@@ -2,20 +2,49 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const beats = {
+    rock: "scissors",
+    paper: "rock",
+    scissor: "paper"
+}
+
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     switch(choice) {
         case 0: return "rock";
-        case 1: return "scissor";
+        case 1: return "scissors";
         case 2: return "paper";
         default: return "?"
     }
 }
 
 function getHumanChoice() {
-    const input = prompt("Enter your choice:");
+    const input = prompt("Enter your choice (rock, paper, scissors):");
     console.log("You entered:", input);
     return input;
 }
 
-getHumanChoice();
+function playRound() {
+    const human = getHumanChoice().toLowerCase();
+    const computer = getComputerChoice();
+
+    if (human === computer) {
+        console.log("It's a tie!");
+    } else if (beats[human] === computer) {
+        console.log(`you win! ${human} beats ${computer}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computer} beats ${human}`);
+        computerScore++;
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    console.log("Final score:")
+    console.log("You:", humanScore, "Computer:", computerScore);
+}
+
+playGame();
